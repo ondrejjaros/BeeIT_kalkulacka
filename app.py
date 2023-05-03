bdc795bf818090c69fea526fd14c73116a721b6b
On Ja <on.jaros@gmail.com>
2023-05-03 20:17:16 +0200

    pridane11

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 def main():
   jmeno = "Ondrej"
-  print("Hello", jmeno)
+  print("Hello", jmeno, "jak se mas")
 
 if __name__ == "__main__":
   main()
+
+#git add .
+#git commit -m "Upravil jsem main."
+#git push
+
+#git branch nova_feature
+#git switch nova_feature
